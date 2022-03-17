8c82885d3b77dc9c0929fbd03902335df77aac99
Bryleen <bryleen1@live.co.uk>
2022-03-17 16:30:30 +0000

    Python practice

---
+++ file.py
@@ -0,0 +1,12 @@
+# name_variable = input("Enter name ")
+# print("Hello " + name_variable)
+
+# firstname = input("Enter your first name ")
+# lastname = input("Enter your last name ")
+# print("Hello " + firstname + " " + lastname)
+
+# number1 = float(input("Please enter the first number: "))
+# number2 = float(input("Please enter the second number: "))
+# answer = number1 + number2
+# print(number1, "+", number2, "=", answer)
+
---
+++ string_methods.py
@@ -0,0 +1,5 @@
+var1= "Hi Bryleen"
+#print(var1.upper())
+#print(var1.lower())
+#print(var1.replace("Hi", "Hello"))
+print(var1.split())
